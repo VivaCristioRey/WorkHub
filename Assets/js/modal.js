ee09551fd0050c96e421af595368cb2b9625a833
Christian <chris.jara0626@gmail.com>
2025-04-22 18:46:35 -0500

    Se descarga la fuente Rubic y también íconos de Bootstrap, además se termina de construir modal de login y validación de login

---
+++ Assets/js/modal.js
@@ -0,0 +1,23 @@
+//Abrir modal
+var btn = document.getElementById('logIn')
+var modalLogin = document.getElementById('modalLogin')
+
+btn.addEventListener('click', () => {
+    modalLogin.showModal()
+})
+
+//Validar usuario
+let adminUser = {
+    correo: 'admin@usuario.com',
+    contraseña: '123456'
+}
+var sendData = document.getElementById('sendLogIn')
+
+sendData.addEventListener('click', () => {
+    var user = document.getElementById('emailUsr').value
+    var pass = document.getElementById('passUsr').value
+
+    if (user === adminUser.correo && pass === adminUser.contraseña) {
+        window.open('adminPanel.html', '_self')
+    }
+})
